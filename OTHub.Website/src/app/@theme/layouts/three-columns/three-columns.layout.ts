25fe97a0460cbbd5b0e74b2b10bd8d4307d519a1
Luke Skinner <github@lukeskinner.email>
2020-09-13 00:40:48 +0100

    Removed windowed mode so the website works better on widescreens. Tweaks to price factor calculator ui

--- OTHub.Website/src/app/@theme/layouts/three-columns/three-columns.layout.ts
+++ OTHub.Website/src/app/@theme/layouts/three-columns/three-columns.layout.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-three-columns-layout',
   styleUrls: ['./three-columns.layout.scss'],
   template: `
-    <nb-layout windowMode>
+    <nb-layout>
       <nb-layout-header fixed>
         <ngx-header></ngx-header>
       </nb-layout-header>
--- OTHub.Website/src/app/@theme/layouts/two-columns/two-columns.layout.ts
+++ OTHub.Website/src/app/@theme/layouts/two-columns/two-columns.layout.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-two-columns-layout',
   styleUrls: ['./two-columns.layout.scss'],
   template: `
-    <nb-layout windowMode>
+    <nb-layout>
       <nb-layout-header fixed>
         <ngx-header></ngx-header>
       </nb-layout-header>
