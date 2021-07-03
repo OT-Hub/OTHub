bbf4f8505e86b7d65689b089abe8d3f5362ffc55
Luke Skinner <github@lukeskinner.email>
2021-07-03 14:44:08 +0100

    A few basic unit tests

---
+++ OTHub.Tests/TimestampHelperTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OTHub.Settings.Helpers;
+using Xunit;
+
+namespace OTHub.Tests
+{
+    public class TimestampHelperTests
+    {
+        [Fact]
+        public void Timestamp_ValidDate()
+        {
+            DateTime date = TimestampHelper.UnixTimeStampToDateTime(1625319664);
+
+            var expected = new DateTime(2021, 07, 03, 14, 41, 04, DateTimeKind.Local);
+
+            Assert.Equal(date, expected);
+        }
+    }
+}
